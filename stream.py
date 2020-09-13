29e485a729bbb24e9f4d788c76b6817a69eb0801
Aiden Stern <aiden.stern@gmail.com>
2020-09-13 13:01:46 -0400

    stream.py improvements

--- stream.py
+++ stream.py
@@ -1,19 +1,41 @@
-import alpaca_trade_api as tradeapi
-import threading
 from keys import API_KEY, API_SECRET_KEY
+import websocket
+import json
 
 
-conn = tradeapi.stream2.StreamConn(
-    key_id=API_KEY,
-    secret_key=API_SECRET_KEY,
-    base_url='https://paper-api.alpaca.markets',
-    data_url='wss://data.alpaca.markets/stream'
-)
+def on_open(ws):
+    print("[CONNECTION OPENED]")
+    auth_data = {
+        "action": "authenticate",
+        "data": {
+            "key_id": API_KEY,
+            "secret_key": API_SECRET_KEY
+        }
+    }
+
+    ws.send(json.dumps(auth_data))
+
+    trade_symbols = ["T.NFLX", "T.GE"]
+    listen_message = {
+        "action": "listen",
+        "data": {
+            "streams": trade_symbols
+        }
+    }
+
+    ws.send(json.dumps(listen_message))
 
 
-# @conn.on(r'.*')
-# async def on_data(conn, channel, data):
-#     print(data)
+def on_message(ws, message):
+    message = json.loads(message)
+    print(message['data'])
 
-conn.run(['account_updates'])
 
+def on_close(ws):
+    print("[CONNECTION CLOSED]")
+
+
+socket = "wss://data.alpaca.markets/stream"
+alpaca_ws = websocket.WebSocketApp(socket, on_open=on_open, on_message=on_message, on_close=on_close)
+
+alpaca_ws.run_forever()
